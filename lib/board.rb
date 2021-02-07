65e0e50515872f8d798cfd2833a01fc7dfe79032
friendly-designer-db82 <nielsen.j.laura@gmail.com>
2021-02-07 03:01:11 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,9 @@
 # Define the variable board below.
+puts board = [" "," "," "," "," "," "," "," "," "," "]
+def display_board
+  puts row = [" "|" "|" "]
+  puts separator = "___________"
+  puts row
+  puts separator
+  puts row
+end
